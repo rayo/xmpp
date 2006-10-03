18b2e622a0b3181ef8f3db0f5eb87a5d7ee379fa
Peter Saint-Andre <stpeter@jabber.org>
2006-10-03 20:56:41 +0000

    static files only
    git-svn-id: file:///home/ksmith/gitmigration/svn/xmpp/trunk@24 4b5297f7-1745-476d-ba37-a9c6900126ab

--- gen.sh
+++ gen.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
-# rebuilds the entire xmpp.org website
+# copies permanent files for the xmpp.org website
+#
+# NOTEL: after this you still need to run the gen.sh 
+# scripts from the following directories:
+#
+# extensions/
+# internet-drafts/
+# registrar/
+# rfcs/
+# 
 
 basepath=/var/www/stage.xmpp.org
 
-cp about/* $basepath/about/
-cp council/* $basepath/council/
-cp council/agendas/* $basepath/council/agendas/
-./extensions/all.sh
-cp images/* $basepath/images/
-cp includes/* $basepath/includes/
+cp about/*.* $basepath/about/
+cp council/*.* $basepath/council/
+cp council/agendas/*.* $basepath/council/agendas/
+cp council/members/*.* $basepath/council/members/
+cp images/*.* $basepath/images/
+cp includes/*.* $basepath/includes/
 cp index.shtml $basepath/
-./internet-drafts/gen.sh
-./registrar/gen.sh
-./rfcs/gen.sh
-cp schemas/* $basepath/schemas/
+cp schemas/*.* $basepath/schemas/
 cp sorttable.js $basepath/
 cp xmpp.css $basepath/
 
