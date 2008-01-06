d53cbdbf78180a8045c0c782c460c38ddc223de1
Peter Saint-Andre <stpeter@jabber.org>
2008-01-06 05:30:48 +0000

    format changes in progress, including abstracts
    git-svn-id: file:///home/ksmith/gitmigration/svn/xmpp/trunk@1537 4b5297f7-1745-476d-ba37-a9c6900126ab

--- protocols/all.sh
+++ protocols/all.sh
@@ -2,13 +2,13 @@
 # for each manifest, generate an HTML file
 # usage: ./all.sh
 
-protopath=/var/www/xmpp.org/protocols
+path=/var/www/xmpp.org/protocols
 
 ls -d * > protocols.txt
 
 while read f
 do
-    xsltproc proto.xsl $f/manifest.xml > $protopath/$f/index.html
+    xsltproc proto.xsl $f/manifest.xml > $path/$f/index.html
 done < protocols.txt
 
 rm protocols.txt
